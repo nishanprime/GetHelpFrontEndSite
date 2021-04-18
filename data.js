ea471b9be5ee4c985b31193f03edb46fe9e69b44
nishanprime <thapanishan9@gmail.com>
2021-04-18 12:28:14 -0400

    After reactsteap

---
+++ data.js
@@ -0,0 +1,75 @@
+const db= [
+    {
+      name: "HeartAttack",
+      firstEncounterYear: 2010,
+      currentMedicine: "KandoKoJhol",
+      totalCTScans: 4,
+      totalMRIs: 2,
+      lastCTScanDate: "Nov 19,2020",
+      lastMRI: "Oct 23, 2021",
+      CTMRILinks: "https://google.com/",
+      medicalReports: [
+        {
+          date: 2020,
+          hospital: "Yasodha Hospital",
+          doctorName: "Dr. hari Om",
+          reportLink: "https://google.com/",
+          otherAttachments: [
+            {
+              name: "Bill Invoice",
+              link: "https://google.com",
+            },
+          ],
+        },
+      ],
+    },
+    {
+        name: "HeartAttack",
+        firstEncounterYear: 2010,
+        currentMedicine: "KandoKoJhol",
+        totalCTScans: 4,
+        totalMRIs: 2,
+        lastCTScanDate: "Nov 19,2020",
+        lastMRI: "Oct 23, 2021",
+        CTMRILinks: "https://google.com/",
+        medicalReports: [
+          {
+            date: 2020,
+            hospital: "Yasodha Hospital",
+            doctorName: "Dr. hari Om",
+            reportLink: "https://google.com/",
+            otherAttachments: [
+              {
+                name: "Bill Invoice",
+                link: "https://google.com",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "HeartAttack",
+        firstEncounterYear: 2010,
+        currentMedicine: "KandoKoJhol",
+        totalCTScans: 4,
+        totalMRIs: 2,
+        lastCTScanDate: "Nov 19,2020",
+        lastMRI: "Oct 23, 2021",
+        CTMRILinks: "https://google.com/",
+        medicalReports: [
+          {
+            date: 2020,
+            hospital: "Yasodha Hospital",
+            doctorName: "Dr. hari Om",
+            reportLink: "https://google.com/",
+            otherAttachments: [
+              {
+                name: "Bill Invoice",
+                link: "https://google.com",
+              },
+            ],
+          },
+        ],
+      }
+  ],
+   export default db
--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from "./header"
 import Body from "./body"
 import EnterEmail from "./enterEmail"
+
 function app() {
     return (
         <div>
             <Header/>
-            <EnterEmail/>
             <Body/>
         </div>
     )
